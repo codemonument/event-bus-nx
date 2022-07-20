319ae7c997503cd6f5e90227fa845e2be40f2f63
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-20 22:26:17 +0200

    add tests that Events are compareable with instanceof

--- libs/event-bus-core/src/lib/types/bus-event.type.spec.ts
+++ libs/event-bus-core/src/lib/types/bus-event.type.spec.ts
@@ -1,4 +1,22 @@
-import { BusEvent } from "./bus-event.type";
+import { BusEvent, NewableBusEvent } from "./bus-event.type";
+import { SimpleNewable } from "./simple-newable.type";
+
+/**
+ * For testing events without payload
+ */
+class PlainEvent extends BusEvent<void> {
+  public type = "PlainEvent";
+}
+
+/**
+ * For testing events with payload
+ */
+interface DemoPayload {
+  name: string;
+}
+class EventWithPayload extends BusEvent<DemoPayload> {
+  public type = "EventWithPayload";
+}
 
 describe(`bus-event.type`, () => {
   /**
@@ -7,9 +25,6 @@ describe(`bus-event.type`, () => {
    * to *not* provide a param to the constructor when instantiating it.
    */
   it(`should allow events without payload (=void)`, () => {
-    class PlainEvent extends BusEvent<void> {
-      public type = "PlainEvent";
-    }
     const event = new PlainEvent();
     expect(event).toBeDefined();
   });
@@ -19,17 +34,42 @@ describe(`bus-event.type`, () => {
    * the user of EventWithpayload is forced to provide a value of type T.
    */
   it(`should allow events with payload (=any)`, () => {
-    interface DemoDTO {
-      name: string;
-    }
-    class EventWithPayload extends BusEvent<DemoDTO> {
-      public type = "EventWithPayload";
-    }
-
-    const payload = { name: "Bob" };
+    const payload: DemoPayload = { name: "Bob" };
     // Note: If you remove the param, typescript will complain,
     // that you need to provide a value for the payload!
     const event = new EventWithPayload(payload);
     expect(event.payload).toStrictEqual(payload);
   });
+
+  it(`PlainEvent should be compareable with instanceof`, () => {
+    const event = new PlainEvent();
+    expect(event instanceof PlainEvent).toBeTruthy();
+  });
+
+  it(`EventWithPayload should be compareable with instanceof`, () => {
+    const payload: DemoPayload = { name: "Bob" };
+    const event = new EventWithPayload(payload);
+    expect(event instanceof EventWithPayload).toBeTruthy();
+  });
+
+  // TODO: Maybe the NewableXXX Stuff is not testable in this manner (since plain instanceof works directly)
+  // Maybe test this Newable behavior in event-bus.spec
+  //
+  // it.skip(`PlainEvent should be assignable to NewableBusEvent`, () => {
+  //   const event = new PlainEvent();
+  //   const newable: NewableBusEvent<PlainEvent, void> = event;
+  //   // const newable: SimpleNewable<PlainEvent> = event;
+
+  //   expect(newable).toBeDefined();
+  // });
+
+  // it.skip(`EventWithPayload should be assignable to NewableBusEvent`, () => {
+  //   const payload: DemoPayload = { name: "Bob" };
+  //   const event = new EventWithPayload(payload);
+
+  //   const newable: NewableBusEvent<EventWithPayload, DemoPayload> = event;
+  //   // const newable: SimpleNewable<EventWithPayload> = event;
+
+  //   expect(newable).toBeDefined();
+  // });
 });
