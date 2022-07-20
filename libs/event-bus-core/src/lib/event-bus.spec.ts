e1179705fec3dc0d1c644b39b19736934665aac7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-20 20:06:07 +0200

    improve event-bus spec for DemoEvent with instance equal comparison

--- libs/event-bus-core/src/lib/event-bus.spec.ts
+++ libs/event-bus-core/src/lib/event-bus.spec.ts
@@ -1,8 +1,17 @@
 import { EventBus } from "./event-bus";
 import { BusEvent } from "./types/bus-event.type";
+import { BusEventWithPayload } from "./types/bus-event-with-payload.type";
 import { take } from "rxjs";
 
 class DemoEvent extends BusEvent {
+  public type = "DemoEvent";
+}
+
+interface DemoPayload {
+  name: string;
+}
+
+class DemoWithPayload extends BusEventWithPayload<DemoPayload> {
   public type = "DemoEvent";
 }
 
@@ -13,10 +22,20 @@ describe("EventBus", () => {
 
   it("should send and receive (paramless) demo event", (done) => {
     const ebus = new EventBus();
-    ebus.on$(DemoEvent).pipe(take(1)).subscribe(() => {
+    const demoEventInstance = new DemoEvent();
+    ebus.on$(DemoEvent).pipe(take(1)).subscribe((event) => {
+      // Checks object equality
+      expect(event).toBe(demoEventInstance);
       done();
     });
 
-    ebus.emit(new DemoEvent());
+    ebus.emit(demoEventInstance);
+  });
+
+  it.skip("should send and receive DemoWithPayload events", (done) => {
+    const ebus = new EventBus();
+    ebus.on$(DemoWithPayload).pipe(take(1)).subscribe((event) => {
+      done();
+    });
   });
 });
