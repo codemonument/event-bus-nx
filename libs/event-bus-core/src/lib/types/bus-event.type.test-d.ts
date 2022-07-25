308cf4a0c3beabb77213b7544d3fc7de4ae712f4
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-25 13:16:11 +0200

    add tsd type testing tool and stub file for bus-event.type.test-d

---
+++ libs/event-bus-core/src/lib/types/bus-event.type.test-d.ts
@@ -0,0 +1,7 @@
+// Readme about syntax: https://www.npmjs.com/package/tsd
+
+// import { expectType } from "tsd";
+// import concat from ".";
+
+// expectType<string>(concat("foo", "bar"));
+// expectType<string>(concat(1, 2));
