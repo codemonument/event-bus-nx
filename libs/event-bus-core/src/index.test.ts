5b95f59b6bbc581c27a93fe282eec42530900f8f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-27 01:14:47 +0200

    add barebones index.test.ts file

---
+++ libs/event-bus-core/src/index.test.ts
@@ -0,0 +1,8 @@
+// TODO: Figure out, why this import breaks!!!
+// import { EventBus } from "./index";
+
+describe(`index.test - tests the public interface of @codemonument/event-bus-core`, () => {
+  it.skip("should instantiate EventBus", () => {
+    // expect(new EventBus()).toBeDefined();
+  });
+});
