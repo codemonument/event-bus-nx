3454696f9fbe3866cab033b89e30d14d93c0ec3e
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-26 08:45:04 +0200

    delete unused testfile

--- libs/event-bus-core/src/lib/types/bus-event.type.test-d.ts
+++
@@ -1,7 +0,0 @@
-// Readme about syntax: https://www.npmjs.com/package/tsd
-
-// import { expectType } from "tsd";
-// import concat from ".";
-
-// expectType<string>(concat("foo", "bar"));
-// expectType<string>(concat(1, 2));
