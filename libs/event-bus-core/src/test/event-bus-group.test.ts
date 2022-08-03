511b784da63de69c40c21e38035c65562f4b4fd7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-08-03 17:24:36 +0200

    add test for event registration

--- libs/event-bus-core/src/test/event-bus-group.test.ts
+++ libs/event-bus-core/src/test/event-bus-group.test.ts
@@ -1,11 +1,23 @@
-import { EventBus } from '../lib/event-bus/event-bus';
-import { EventBusGroup } from '../lib/event-bus-group/event-bus-group';
+import { EventBus } from "../lib/event-bus/event-bus";
+import { EventBusGroup } from "../lib/event-bus-group/event-bus-group";
+import { PlainEvent } from "./test-events.types";
 
 describe(`event-bus-group.test`, () => {
   it(`should be constructed`, () => {
     const eBus = new EventBus();
     const eGroup = new EventBusGroup(eBus);
     expect(eGroup).toBeDefined();
+  });
+
+  it(`should receive PlainEvent in callback (test event registration)`, (done) => {
+    const eBus = new EventBus();
+    const eGroup = new EventBusGroup(eBus);
+
+    eGroup.on(PlainEvent, () => {
+      done();
+    });
+
+    eBus.emit(new PlainEvent());
   });
 
   // TODO: Finish EventBusGroup Tests!
