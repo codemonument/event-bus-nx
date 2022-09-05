810ef761d81fb4d225d194b44b15567939f56c46
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-05 21:20:57 +0200

    re-enable test because it works now

--- libs/event-bus-core/src/index.test.ts
+++ libs/event-bus-core/src/index.test.ts
@@ -1,8 +1,7 @@
-// TODO: Figure out, why this import breaks!!!
-// import { EventBus } from "./index";
+import { EventBus } from "./index";
 
 describe(`index.test - tests the public interface of @codemonument/event-bus-core`, () => {
-  it.skip('should instantiate EventBus', () => {
-    // expect(new EventBus()).toBeDefined();
+  it("should instantiate EventBus", () => {
+    expect(new EventBus()).toBeDefined();
   });
 });
